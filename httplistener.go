002e05cebff2494f1cdd036a0ecbc42253ac0961
Marc Sluiter <marc.sluiter@luminis.eu>
2016-03-03 16:32:25 +0100

    RTI-460 removed updateDeployment, not used and not needed

--- httplistener.go
+++ httplistener.go
@@ -55,7 +55,6 @@ func main() {
 
 	r := mux.NewRouter()
 	r.HandleFunc("/deployments/{namespace}", listDeployments).Methods("GET")
-	r.HandleFunc("/deployments/{namespace}/{id}", updateDeployment).Methods("PUT")
 	r.HandleFunc("/deployments/{namespace}/{id}", deleteDeployment).Methods("DELETE")
 	r.HandleFunc("/deployment", DeploymentHandler).Methods("POST")
 
@@ -92,37 +91,6 @@ func listDeployments(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Write(jsonStr)
-}
-
-func updateDeployment(w http.ResponseWriter, r *http.Request) {
-	registry, err := createDeploymentRegistry(w, r)
-	if err != nil {
-		return
-	}
-
-	body, err := ioutil.ReadAll(r.Body)
-
-	if err != nil {
-		w.WriteHeader(500)
-		io.WriteString(w, "Error parsing deployment: "+err.Error())
-		return
-	}
-
-	deployment, err := deploymentregistry.ParseDeployment(string(body))
-
-	if err != nil {
-		w.WriteHeader(500)
-		io.WriteString(w, "Error parsing deployment: "+err.Error())
-		return
-	}
-
-	err = registry.StoreDeployment(deployment)
-
-	if err != nil {
-		w.WriteHeader(500)
-		io.WriteString(w, "Error storing deployment: "+err.Error())
-		return
-	}
 }
 
 func deleteDeployment(w http.ResponseWriter, r *http.Request) {
